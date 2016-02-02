d7579233ba60629ea39d24b6549bda5e75dbb7c8
Benoît Burnichon <bburnichon@alchemy.fr>
2016-02-02 15:14:25 +0100

    Add storage_zones tables and column

---
+++ lib/classes/patch/400alpha6a.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of Phraseanet
+ *
+ * (c) 2005-2016 Alchemy
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use Alchemy\Phrasea\Application;
+
+class patch_400alpha6a implements patchInterface
+{
+    /** @var string */
+    private $release = '4.0.0-alpha.6';
+
+    /** @var array */
+    private $concern = [base::DATA_BOX];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get_release()
+    {
+        return $this->release;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDoctrineMigrations()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function require_all_upgrades()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function concern()
+    {
+        return $this->concern;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(base $databox, Application $app)
+    {
+        return true;
+    }
+}
